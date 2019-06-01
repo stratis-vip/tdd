b7d60933c6a391043e6ed17d1855cca12a0e2336
stratis <stratis.vip@gmail.com>
2019-06-01 08:39:10 +0300

    remove 'src' 'lib' folders

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const toRoman = require('./romanarabic').arabicToRomanNumbers;
-const toArabic = require('./romanarabic').romanToArabicNumbers;
-
-console.log(toRoman(45));
-
-console.log(toArabic('nulla'));
